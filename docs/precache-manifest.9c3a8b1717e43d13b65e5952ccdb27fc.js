ad47781b8ee1321f279be2dfd2add7ca7c2921be
Brando <brandto.tomalav@outlook.com>
2021-01-25 18:55:35 -0500

    Add docs

---
+++ docs/precache-manifest.9c3a8b1717e43d13b65e5952ccdb27fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4518c2870388ad45511a3fd29f06f60d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39c8b34bd73baba3f124",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "3988cfc22d0bf0f65000",
+    "url": "/static/css/main.c33cc627.chunk.css"
+  },
+  {
+    "revision": "39c8b34bd73baba3f124",
+    "url": "/static/js/2.6e2a0b08.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.6e2a0b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3988cfc22d0bf0f65000",
+    "url": "/static/js/main.f4abf379.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
